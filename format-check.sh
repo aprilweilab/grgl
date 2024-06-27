4b0d4ff4502c41f23c30d323cda6e39c8b10d778
Drew DeHaas <drewdehaas@gmail.com>
2024-06-27 16:06:12 -0400

    Add format check script (for GitHub workflow)

---
+++ format-check.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -ev
+
+clang-format --dry-run -Werror src/*.cpp
+clang-format --dry-run -Werror src/python/*.cpp
+clang-format --dry-run -Werror include/grgl/*.h
---
+++ format.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -ev
+
+clang-format -i src/*.cpp
+clang-format -i src/python/*.cpp
+clang-format -i include/grgl/*.h
