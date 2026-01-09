60c98436b8b80b778299a5f79519908a43a41328
Drew DeHaas <drewdehaas@gmail.com>
2026-01-09 14:16:25 -0500

    Improve matmul's missingness support
    The "miss=M" parameter is now a matrix that matches the dimension
    of either the input or the output matrix, depending on the
    direction of the matmul().
    
    Add an end-to-end test that illustrates how to use this matrix to
    mimic the explicit genotype matrix with a GRG. With an explicit
    matrix we commonly set all missing values to the mean, e.g. the
    allele frequency for the given mutation. Equivalent results can
    be obtained with the GRG in both directions (UP and DOWN), e.g.
    AX and AX^T using the "miss=..." parameter.

--- test/endtoend/testing_utils.py
+++ test/endtoend/testing_utils.py
@@ -1,5 +1,7 @@
 import subprocess
 import os
+import pygrgl
+import numpy
 from typing import Optional
 
 JOBS = 4
@@ -29,3 +31,71 @@ def construct_grg(
         output_file = os.path.basename(input_file) + ".final.grg"
     subprocess.check_call(cmd)
     return output_file
+
+
+def allele_frequencies(grg: pygrgl.GRG) -> numpy.typing.NDArray:
+    with numpy.errstate(divide="raise"):
+        kwargs = {}
+        miss = 0
+        if grg.has_missing_data:
+            miss = numpy.zeros((1, grg.num_mutations), dtype=numpy.int32)
+            kwargs["miss"] = miss
+        else:
+            miss = None
+        counts = pygrgl.matmul(
+            grg,
+            numpy.ones((1, grg.num_samples), dtype=numpy.int32),
+            pygrgl.TraversalDirection.UP,
+            **kwargs,
+        )[0]
+        if miss is None:
+            miss = 0
+        else:
+            miss = miss[0]
+        denominator = grg.num_samples - miss
+        return numpy.divide(
+            counts,
+            denominator,
+            out=numpy.zeros(counts.shape, dtype=numpy.float64),
+            where=(denominator != 0),
+        )
+
+
+# It is important that this only uses down edges, so that we can test against
+# matmul methods which (should) only require down edges.
+def grg2X(grg: pygrgl.GRG, diploid: bool = False):
+    samples = grg.num_individuals if diploid else grg.num_samples
+    result = numpy.zeros((samples, grg.num_mutations))
+    samps_below = [list() for _ in range(grg.num_nodes)]
+    for node_id in range(grg.num_nodes):
+        sb = []
+        if grg.is_sample(node_id):
+            sb.append(node_id)
+        for child_id in grg.get_down_edges(node_id):
+            sb.extend(samps_below[child_id])
+        samps_below[node_id] = sb
+
+        muts = grg.get_mutations_for_node(node_id)
+        if muts:
+            for sample_id in sb:
+                indiv = sample_id // grg.ploidy
+                for mut_id in muts:
+                    if diploid:
+                        result[indiv][mut_id] += 1
+                    else:
+                        result[sample_id][mut_id] = 1
+    # Handle missingness afterwards for simplicity (harder to make mistakes this way).
+    if grg.has_missing_data:
+        freqs = allele_frequencies(grg)
+        for mut_id, mut_node, miss_node in grg.get_mutation_node_miss():
+            if miss_node != pygrgl.INVALID_NODE:
+                for sample_id in samps_below[miss_node]:
+                    if diploid:
+                        indiv = sample_id // grg.ploidy
+                        result[indiv][mut_id] += freqs[mut_id]
+                    else:
+                        assert (
+                            result[sample_id][mut_id] == 0
+                        ), f"{result[sample_id][mut_id]}"
+                        result[sample_id][mut_id] = freqs[mut_id]
+    return result
